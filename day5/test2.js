69ac0357b8977a947e139d5fbe315e3a159ecfe8
akashverma07 <vakash789@gmail.com>
2018-01-17 15:52:17 +0530

    end of chunky monkey

--- day5/test2.js
+++ day5/test2.js
@@ -1,17 +1,17 @@
 function chunkArrayInGroups(arr, size) {
     var ar = [];
+    var b = arr.length % size;
     var temp;
-    for(var i=0 ; i<arr.length-1 ; i+=size){
+    for(var i=0 ; i<arr.length-b ; i+=size){
         ar.push(arr.slice(i,i+size));
     }
-    var b = arr.length % size;
     if(b != 0){
         console.log(b);
         ar.push(arr.slice(arr.length-b,arr.length));
-    }
+    } 
   console.log(ar);
    
   }
   
-  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);
+  chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);
   
