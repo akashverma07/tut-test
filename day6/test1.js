9ef34e5c261e4a95928aaf897894d81d8bf8b921
akashverma07 <vakash789@gmail.com>
2018-01-19 19:13:46 +0530

    day5/test4/incomplete

---
+++ day6/test1.js
@@ -0,0 +1,11 @@
+// checking the variable is empty or not
+var a;
+if(a || a===0){
+    console.log('this contains value');
+}
+else{
+    console.log('this is empty');
+}
+
+//----------------------------------------------------------------------------
+
